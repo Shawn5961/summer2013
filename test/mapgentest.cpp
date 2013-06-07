0ecfebdf5ff998329c6609090821c962f9589d01
Shawn Meas <shawn.meas89@gmail.com>
2013-06-07 16:54:30 -0400

    More work on mapgen

--- test/mapgentest.cpp
+++ test/mapgentest.cpp
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define WALL	0
+#define PATH	1
+#define START	2
+#define END		3
+
 int main()
 {
 	srand(time(NULL));
 
-	int x = 32;
-	int y = 24;
+	int x, y;
 
-	char grid[32][24];
+	int grid[32][24];
 	int startX, startY, endX, endY;
 	int loopcounter = 0;
 
@@ -17,39 +21,47 @@ int main()
 
 	do
 	{
-		startX = rand() % x;
-		startY = rand() % y;
+		startX = rand() % 30 + 1;
+		startY = rand() % 22 + 1 ;
 
-		endX = rand() % x;
-		endY = rand() % y;
+		endX = rand() % 30 + 1;
+		endY = rand() % 22 + 1;
 
 		loopcounter++;
-	}while( startX == endX && startY == endY);
+	}while( (startX == endX && startY == endY) );
 
-	for( i = 0; i < y; i++ )
+	for( y = 0; y < 24; y++ )
 	{
-		grid[0][i] = '#';
-		grid[x - 1][i] = '#';
-	}
-
-	for( j = 0; j < x; j++ )
-	{
-		grid[j][0] = '#';
-		grid[j][y - 1] = '#';
-	}
-
-	grid[startX][startY] = 's';
-	grid[endX][endY] = 'e';
-
-	for( i = 0; i < y; i++ )
-	{
-		for( j = 0; j < x; j++ )
+		for( x = 0; x < 32; x++ )
 		{
-			if( grid[j][i] == '#' )
+			grid[x][y] = 1;
+		}
+	}
+
+	for( x = 0; x < 32; x++ )
+	{
+		grid[x][0] = 0;
+		grid[x][23] = 0;
+	}
+
+	for( y = 0; y < 24; y++ )
+	{
+		grid[0][y] = 0;
+		grid[31][y] = 0;
+	}
+
+	grid[startX][startY] = 2;
+	grid[endX][endY] = 3;
+
+	for( y = 0; y < 24; y++ )
+	{
+		for( x = 0; x < 32; x++ )
+		{
+			if( grid[x][y] ==  0)
 				printf("#");
-			else if( grid[j][i] == 's' )
+			else if( grid[x][y] == 2 )
 				printf("s");
-			else if( grid[j][i] == 'e' )
+			else if( grid[x][y] == 3 )
 				printf("e");
 			else
 				printf(" ");
